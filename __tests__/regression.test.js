5a1c7cdfd5896ca24ec7648cf94a8a88066365c5
Dave Stockley <magicspon@gmail.com>
2019-06-06 16:46:51 +0100

    added cypress e2e testing

--- __tests__/regression.test.js
+++ __tests__/regression.test.js
@@ -3,5 +3,5 @@ import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 
 initStoryshots({
 	suite: 'Image storyshots',
-	test: imageSnapshot({ storybookUrl: 'http://localhost:9001/' })
+	test: imageSnapshot({ storybookUrl: 'http://localhost:6006/' })
 })
