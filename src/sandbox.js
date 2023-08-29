8eb9936a3acd547cd4ec9cd0ac3c0993807ae87b
David Stockley <magicspon@gmail.com>
2023-08-29 17:12:29 +0100

    new site

--- src/sandbox.js
+++
@@ -1,5 +0,0 @@
-const details = {
-	Client: { name: 'Search Star', link: 'https://www.search-star.co.uk' },
-	Branding: { name: 'Supple Studios', link: 'http://www.supplestudio.com/' },
-	'Agency/Design': { name: 'Mud', link: 'https://ournameismud.co.uk/' }
-}
