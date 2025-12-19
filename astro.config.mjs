a3ca98b326234a024851c771db85ea22024645ef
David Stockley <magicspon@gmail.com>
2025-12-19 16:51:52 +0000

    refactor: make to freelance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  compressHTML: true,
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  output: 'hybrid'
+	integrations: [tailwind()],
+	compressHTML: true,
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
